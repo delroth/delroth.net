77e125796155eb77084fd933e794bd16c17e51f1
Pierre Bourdon <delroth@gmail.com>
2023-06-06 04:33:15 +0200

    nix: export overlay in flake

--- flake.nix
+++ flake.nix
@@ -4,16 +4,23 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlay = final: prev: {
+      delroth-net-website = prev.callPackage ./default.nix {};
+    };
+  } // (flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
     in rec {
-      packages.delroth-net-website = pkgs.callPackage ./default.nix {};
-      defaultPackage = packages.delroth-net-website;
+      packages.delroth-net-website = pkgs.delroth-net-website;
+      defaultPackage = pkgs.delroth-net-website;
 
       devShells.default = with pkgs; mkShell {
         buildInputs = [ go hugo ];
       };
     }
-  );
+  ));
 }
